7e83e5b9eb601bafec0b30abb0f87fc2027ed34f
LucasFelix-S <started.lucasfelixsantos@gmail.com>
2026-02-22 18:51:31 -0300

    feat(local-estoque): adiciona entidade LocalEstoque
    Inclui novo modelo no domínio da aplicação
    para controle de localizadores de estoque.
    
    Signed-off-by: Lucas Felix Santos <started.lucasfelixsantos@gmail.com>

---
+++ src/main/java/io/chronoslbm/wms/domain/model/LocalEstoque.java
@@ -0,0 +1,69 @@
+package io.chronoslbm.wms.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+public class LocalEstoque {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "codigo")
+    @NotNull
+    @NotBlank
+    private String codigo;
+
+    @Column(name = "descricao")
+    @NotNull
+    @NotBlank
+    private String descricao;
+
+    @Column(name = "rua")
+    @NotNull
+    @NotBlank
+    private String rua;
+
+    @Column(name = "modulo")
+    @NotNull
+    @NotBlank
+    private String modulo;
+
+    @Column(name = "nivel")
+    @NotNull
+    @NotBlank
+    private String nivel;
+
+    @Column(name = "container")
+    @NotNull
+    @NotBlank
+    private String container;
+
+    @Column(name = "zona_estoque_id")
+    @NotNull
+    @NotBlank
+    private Long zonaEstoqueId;
+
+    @Column(name = "setor_id")
+    @NotNull
+    @NotBlank
+    private Long setorId;
+
+    @Column(name = "status_id")
+    @NotNull
+    @Min(1)
+    @Max(2)
+    private Long statusId;
+}
