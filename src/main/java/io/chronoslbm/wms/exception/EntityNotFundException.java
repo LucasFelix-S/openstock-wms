99f3db2799582c76a2aaa87b218bd4aba7e18dd8
LucasFelix-S <started.lucasfelixsantos@gmail.com>
2026-02-26 21:53:43 -0300

    feat(exception): adiciona EntityNotFoundException global
    Cria exceção personalizada para tratamento de entidades
    não encontradas no domínio e regras da aplicação.
    
    Permite padronizar erros de consulta e melhorar
    consistência nas respostas da API.
    Signed-off-by: Lucas Felix Santos <started.lucasfelixsantos@gmail.com>

---
+++ src/main/java/io/chronoslbm/wms/exception/EntityNotFundException.java
@@ -0,0 +1,7 @@
+package io.chronoslbm.wms.exception;
+
+public class EntityNotFundException extends RuntimeException {
+    public EntityNotFundException(String message) {
+        super(message);
+    }
+}
